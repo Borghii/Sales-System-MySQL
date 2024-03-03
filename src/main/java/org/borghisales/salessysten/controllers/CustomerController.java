5a642dbec11df1fb11d97a0d685e0cc6dbd81bdb
Borghii <137845283+Borghii@users.noreply.github.com>
2024-03-02 21:01:21 -0300

    fea: add CRUD Customers

--- src/main/java/org/borghisales/salessysten/controllers/CustomerController.java
+++ src/main/java/org/borghisales/salessysten/controllers/CustomerController.java
@@ -1,5 +1,9 @@
 package org.borghisales.salessysten.controllers;
 
+import javafx.application.Platform;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -10,12 +14,15 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import org.borghisales.salessysten.model.Customer;
-import org.borghisales.salessysten.model.Seller;
+import org.borghisales.salessysten.model.CustomerDAO;
+
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class CustomerController implements Initializable {
+
+    CustomerDAO customerDAO = new CustomerDAO();
 
     ObservableList<Customer.State> stateList = FXCollections.observableArrayList(Customer.State.ACTIVE, Customer.State.DISACTIVE);
 
@@ -45,20 +52,68 @@ public class CustomerController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         tableCustomers.setOnMouseClicked(mouseEvent->{
-
+            if (!tableCustomers.getSelectionModel().isEmpty() && mouseEvent.getClickCount()==2){
+                Customer customer = tableCustomers.getSelectionModel().getSelectedItem();
+                setCells(customer);
+            }
         });
+
+        Platform.runLater(() -> {
+            cbState.setValue(Customer.State.ACTIVE);
+            cbState.setItems(stateList);
+
+            customers = FXCollections.observableArrayList();
+
+            colId.setCellValueFactory(p -> new SimpleIntegerProperty(p.getValue().idCustomer()).asObject());
+            colName.setCellValueFactory(p -> new SimpleStringProperty(p.getValue().name()));
+            colDni.setCellValueFactory(p-> new SimpleStringProperty(p.getValue().dni()));
+            colAdress.setCellValueFactory(p-> new SimpleStringProperty(p.getValue().address()));
+            colState.setCellValueFactory(p -> new SimpleObjectProperty<Customer.State>(p.getValue().state()));
+
+            tableCustomers.getItems().clear();
+            customerDAO.setTable(customers);
+            tableCustomers.setItems(customers);
+        });
+
     }
     @FXML
     public void addCustomer(ActionEvent actionEvent) {
+        Customer customer = new Customer(dni.getText(),name.getText(),address.getText(),cbState.getValue());
+        if (customerDAO.create(customer)) {
+            MenuController.cleanCells(dni, name, address);
+            updateTable();
+        }
     }
     @FXML
     public void updateCustomer(ActionEvent actionEvent) {
+        Customer customer = new Customer(dni.getText(),name.getText(),address.getText(),cbState.getValue());
+        if (customerDAO.update(customer)) {
+            MenuController.cleanCells(dni, name, address);
+            updateTable();
+        }
     }
     @FXML
     public void deleteCustomer(ActionEvent actionEvent) {
+        if (customerDAO.delete(dni.getText())){
+            MenuController.cleanCells(dni,name,address);
+            updateTable();
+        }
     }
     @FXML
     public void cleanCellsScreen(ActionEvent actionEvent) {
+        MenuController.cleanCells(dni,name,address);
+    }
+    private void setCells(Customer customer){
+        name.setText(customer.name());
+        dni.setText(customer.dni());
+        address.setText(customer.address());
+        cbState.setValue(customer.state());
+    }
+
+    private void updateTable() {
+        tableCustomers.getItems().clear();
+        customerDAO.setTable(customers);
+        tableCustomers.setItems(customers);
     }
 
 
--- src/main/java/org/borghisales/salessysten/model/CustomerDAO.java
+++ src/main/java/org/borghisales/salessysten/model/CustomerDAO.java
@@ -1,11 +1,41 @@
 package org.borghisales.salessysten.model;
 
 import javafx.collections.ObservableList;
+import javafx.scene.control.Alert;
+import org.borghisales.salessysten.controllers.MenuController;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class CustomerDAO implements CRUD<Customer> {
     @Override
     public boolean create(Customer entity) {
-        return false;
+        String sql = "Insert into customer (dni,name,address,state) values(?,?,?,?)";
+
+        try (Connection conn = DBConnection.connection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)){
+
+            pstmt.setString(1, entity.dni());
+            pstmt.setString(2, entity.name());
+            pstmt.setString(3, entity.address());
+            pstmt.setString(4, entity.state().toString());
+
+            int rows_affected = pstmt.executeUpdate();
+
+            if (rows_affected>0){
+                MenuController.setAlert(Alert.AlertType.CONFIRMATION, "Customer added correctly");
+                return true;
+            }else{
+                MenuController.setAlert(Alert.AlertType.ERROR, "Error adding customer: ");
+                return false;
+            }
+
+        }catch (SQLException e){
+            MenuController.setAlert(Alert.AlertType.ERROR, "Error adding customer: " + e.getMessage());
+            return false;
+        }
     }
 
     @Override
@@ -15,16 +45,86 @@ public class CustomerDAO implements CRUD<Customer> {
 
     @Override
     public boolean update(Customer entity) {
-        return false;
+        String sql = "UPDATE customer set name=?,address=?,state=? where dni=?";
+
+        try(Connection conn = DBConnection.connection();
+            PreparedStatement pstmt = conn.prepareStatement(sql)){
+
+
+
+            pstmt.setString(1, entity.name());
+            pstmt.setString(2, entity.address());
+            pstmt.setString(3, entity.state().name());
+            pstmt.setString(4, entity.dni());
+
+
+
+
+            int rows_affected = pstmt.executeUpdate();
+
+            if (rows_affected>0){
+                MenuController.setAlert(Alert.AlertType.CONFIRMATION, "Customer updated correctly");
+                return true;
+            }else{
+                MenuController.setAlert(Alert.AlertType.ERROR, "Error updating customer");
+                return false;
+            }
+
+        }catch (SQLException e){
+            MenuController.setAlert(Alert.AlertType.ERROR, "Error updating customer: " + e.getMessage());
+            return false;
+        }
     }
 
     @Override
     public boolean delete(String id) {
-        return false;
+        String sql = "DELETE FROM customer where dni=?";
+
+        try(Connection conn = DBConnection.connection();
+            PreparedStatement pstmt = conn.prepareStatement(sql))    {
+
+
+            pstmt.setString(1,id);
+
+            int rows_affected = pstmt.executeUpdate();
+
+            if (rows_affected>0){
+                MenuController.setAlert(Alert.AlertType.CONFIRMATION, "Customer deleted correctly");
+                return true;
+            }else{
+                MenuController.setAlert(Alert.AlertType.ERROR, "Error deleting customer: ");
+                return false;
+            }
+
+
+
+        }catch (SQLException e){
+            MenuController.setAlert(Alert.AlertType.ERROR, "Error deleting customer: " + e.getMessage());
+            return false;
+        }
     }
 
     @Override
-    public void setTable(ObservableList<Customer> table) {
+    public void setTable(ObservableList<Customer> customers) {
+        String sql = "SELECT * FROM customer";
+
+        try (Connection conn = DBConnection.connection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)){
+
+            try (ResultSet rs = pstmt.executeQuery()){
+
+                while (rs.next()){
+                    Customer customer = new Customer(rs.getInt("idCustomer"),rs.getString("dni"),
+                            rs.getString("name"), rs.getString("address"),
+                            Customer.State.valueOf(rs.getString("state")));
+
+                    customers.add(customer);
+                }
+
+            }
+        }catch (SQLException e){
+            MenuController.setAlert(Alert.AlertType.ERROR, "Error setting the table customer: " + e.getMessage());
+        }
 
     }
 }
